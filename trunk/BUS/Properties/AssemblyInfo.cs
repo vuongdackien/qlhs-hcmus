b665af3967ab9b180d0eee9024c4368cf51724e5
mydien2009@gmail.com <mydien2009@gmail.com@56091531-0d08-3e6d-6f4e-de06c65f6e93>
2012-05-04 03:17:26 +0000

    Them BUS

--- trunk/BUS/Properties/AssemblyInfo.cs
+++ trunk/BUS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59294ba3-7281-464a-843b-44bf01086a11")]
+[assembly: Guid("dd38e2fb-6011-4880-88b9-82934a195aad")]
 
 // Version information for an assembly consists of the following four values:
 //
