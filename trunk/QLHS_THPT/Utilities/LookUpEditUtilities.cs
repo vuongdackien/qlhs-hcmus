fdb7905f9daa88f5a861dc282dd1b513bd779256
hongphu8790@gmail.com <hongphu8790@gmail.com@56091531-0d08-3e6d-6f4e-de06c65f6e93>
2012-05-14 16:01:44 +0000

    Thêm các lớp Utilities: ComboboxEdit, MessageBox, LookUpEdit

---
+++ trunk/QLHS_THPT/Utilities/LookUpEditUtilities.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using QLHS.DTO;
+using DevExpress.XtraEditors;
+
+namespace QLHS_THPT
+{
+
+    public class LookUpEditUtilities
+    {
+
+        /// <summary>
+        /// Gắn DataSource cho LookUpEdit
+        /// </summary>
+        /// <param name="lookUpEdit">LookUpEdit</param>
+        /// <param name="tb">DataTable</param>
+        /// <param name="nameValue">String: Value member</param>
+        /// <param name="nameDisplay">String: Display member</param>
+        /// <param name="select_index">Int: Chọn dòng</param>
+        public static void SetDataSource
+                (LookUpEdit lookUpEdit, DataTable tb, string nameValue, string nameDisplay, int select_index = 0)
+        {
+            // add datasource
+            lookUpEdit.Properties.DataSource = tb;
+            lookUpEdit.Properties.DisplayMember = nameDisplay;
+            lookUpEdit.Properties.ValueMember = nameValue;
+            // seleted index
+            lookUpEdit.ItemIndex = select_index;
+        }
+        /// <summary>
+        /// Gắn DataSource cho LookUpEdit
+        /// </summary>
+        /// <param name="lookUpEdit">LookUpEdit</param>
+        /// <param name="tb">DataTable</param>
+        /// <param name="nameValue">String: Value member</param>
+        /// <param name="nameDisplay">String: Display member</param>
+        /// <param name="nameValue_all">String: Display member dòng đầu tiên</param>
+        /// <param name="nameDisplay_all">String: Value member dòng đầu tiên</param>
+        /// <param name="select_index">Int: Chọn dòng</param>
+        public static void SetDataSource
+         (LookUpEdit lookUpEdit, DataTable tb, string nameValue, string nameDisplay,
+                   string nameValue_all = "all", string nameDisplay_all = "Tất cả", int select_index = 0)
+        {
+            // Add new row
+            DataRow dr = tb.NewRow();
+            dr[nameValue] = nameValue_all;
+            dr[nameDisplay] = nameDisplay_all;
+            tb.Rows.InsertAt(dr, 0);
+            // Add datasource
+            lookUpEdit.Properties.DataSource = tb;
+            lookUpEdit.Properties.DisplayMember = nameDisplay;
+            lookUpEdit.Properties.ValueMember = nameValue;
+            // seleted index
+            lookUpEdit.ItemIndex = select_index;
+        }
+
+        /// <summary>
+        /// Lấy giá trị selected của LookUpEdit
+        /// </summary>
+        /// <param name="lookUpEdit">LookUpEdit</param>
+        /// <param name="ValueMember">String: tên field ValueMember muốn lấy</param>
+        /// <returns>String: Giá trị valuemember</returns>
+        public static string GetValueItem(LookUpEdit lookUpEdit, string ValueMember)
+        {
+            return lookUpEdit.GetColumnValue(ValueMember).ToString();
+        }
+
+        /// <summary>
+        /// Chọn Item ComboBoxEdit
+        /// </summary>
+        /// <param name="comb">ComboBoxEdit</param>
+        /// <param name="svalue">String: Giá trị chọn valuemember</param>
+        public static void SelectedItem(LookUpEdit lookUpEdit, string svalue)
+        {
+            lookUpEdit.EditValue = svalue;
+        }
+       
+
+      
+    }
+}
