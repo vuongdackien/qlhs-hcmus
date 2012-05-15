67e0d684431ac6a96e16dd23d5aa69ebcf5fe938
mydien2009@gmail.com <mydien2009@gmail.com@56091531-0d08-3e6d-6f4e-de06c65f6e93>
2012-05-15 11:28:12 +0000

    Thêm form TongKetHocKy, TongKetNamHoc, Test Report

---
+++ trunk/QLHS_THPT/frmTongKetHocKy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QLHS.Report;
+using QLHS.BUS;
+using QLHS.DTO;
+
+namespace QLHS
+{
+    public partial class frmTongKetHocKy : DevExpress.XtraEditors.XtraForm
+    {
+        private BangDiemBUS _BangDiemBUS = new BangDiemBUS();
+        public frmTongKetHocKy()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButtonXuatBD_Click(object sender, EventArgs e)
+        {
+            var ds = _BangDiemBUS.LayDTDiem_HocKy_Lop("10A01NH1112", "1");
+            var rp = new rptBangDiem();
+            rp.SetDataSource(ds);
+
+            frmReportView_TongKetHK _frmReportView_TongKetHK = new frmReportView_TongKetHK();
+            _frmReportView_TongKetHK.crystalReportViewerTongKetMonHoc.ReportSource = rp;
+            _frmReportView_TongKetHK.ShowDialog();
+        }
+    }
+}
