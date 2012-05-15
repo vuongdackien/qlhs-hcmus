cbadb57b5be74020d54c0b3b1d906c74e46f65cb
nguyendai292@gmail.com <nguyendai292@gmail.com@56091531-0d08-3e6d-6f4e-de06c65f6e93>
2012-05-15 19:24:33 +0000

    Update quy dinh DTO

--- trunk/DTO/QuyDinhDTO.cs
+++ trunk/DTO/QuyDinhDTO.cs
@@ -6,5 +6,73 @@ namespace QLHS.DTO
 {
     public class QuyDinhDTO
     {
+       
+	
+    private int TuoiCD;
+
+	public int TuoiCanDuoi
+	{
+		get { return TuoiCD;}
+		set { TuoiCD = value;}
+	}
+	private int TuoiCT;
+
+	public int TuoiCanTren
+	{
+		get { return TuoiCT;}
+		set { TuoiCT = value;}
+	}
+    private int SiSoCD;
+
+	public int SiSoCanDuoi
+	{
+		get { return SiSoCD;}
+		set { SiSoCD = value;}
+	}
+	 private int SiSoCT;
+
+	public int SiSoCanTren
+	{
+		get { return SiSoCT;}
+		set { SiSoCT = value;}
+	}
+	private int SoMH;
+
+	public int SoMonHoc
+	{
+		get { return SoMH;}
+		set { SoMH = value;}
+	}
+	private int SoLuongL;
+
+	public int SoLuongLop
+	{
+		get { return SoLuongL;}
+		set { SoLuongL = value;}
+	}
+    private decimal DiemCH;
+
+	public decimal DiemChuan
+	{
+		get { return DiemCH;}
+		set { DiemCH = value;}
+	}
+	private String TenTR;
+
+	public String TenTruong
+	{
+		get { return TenTR;}
+		set { TenTR = value;}
+	}
+	
+	private string DiaChiTR;
+
+	public string DiaChiTruong
+	{
+		get { return DiaChiTR;}
+		set { DiaChiTR = value;}
+	}
+	
+	
     }
 }
