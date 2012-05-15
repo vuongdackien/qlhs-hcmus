2973028248a62e3236a82b4640ba28feff6033a2
nguyendai292 <nguyendai292@gmail.com>
2012-05-15 18:31:47 +0000

    Comit GiaoVienVDTO

--- DTO/GiaoVienDTO.cs
+++ DTO/GiaoVienDTO.cs
@@ -6,5 +6,23 @@ namespace QLHS.DTO
 {
     public class GiaoVienDTO
     {
+      
+        private string  MaGiaoVien;
+
+        public string MaGV
+        {
+            get { return MaGiaoVien; }
+            set { MaGiaoVien = value; }
+        }
+        private string TenGiaoVien;
+
+	    public string TenGV        
+	    {
+		    get { return TenGiaoVien;}
+		    set { TenGiaoVien = value;}
+	    }
+	
+        
+
     }
 }
