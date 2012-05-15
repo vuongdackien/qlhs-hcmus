036346496342c235a6798a86865c41b12b547b47
hongphu8790 <hongphu8790@gmail.com>
2012-05-15 09:31:02 +0000

    Thêm form giáo viên

---
+++ QLHS_THPT/frmGiaoVien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QLHS
+{
+    public partial class frmGiaoVien : DevExpress.XtraEditors.XtraForm
+    {
+        public frmGiaoVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
